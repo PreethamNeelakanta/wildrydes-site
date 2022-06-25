cc14a5faca1cba21554cd882f9396d1d295c0697
PreethamNeelakanta <100431891+PreethamNeelakanta@users.noreply.github.com>
2022-06-25 18:58:25 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-west-2_uXboG5pAb', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '25ddkmj4v6hfsfvruhpfi7n4hv', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-2_O3bNq83f7', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '51sd9p39q3rmhqg2vdurjar3vq', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-west-2' // e.g. us-east-2
     },
     api: {
